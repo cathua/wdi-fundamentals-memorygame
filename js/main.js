94433df2f55b90e533af49dc8863437832ad1270
Hua <stc793@dca9047624b5.cof.ds.capitalone.com>
2017-07-21 10:02:31 -0400

    updated the js file to reflect changes made after unit 9.

--- js/main.js
+++ js/main.js
@@ -1,21 +1,23 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
 
-cardsInPlay.push(cardOne)
-
-console.log("User flipped " + cardOne)
-
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardTwo)
-
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]){
-    alert("You found a match! Nice! (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧");
-  }
-  else{
-    alert("Sorry, try again (◡△◡✿)");
+var checkForMatch = function () {
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]){
+      alert("You found a match! Nice! (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧");
+    }
+    else{
+      alert("Sorry, try again (◡△◡✿)");
+    }
   }
 }
+
+
+var flipCard = function (cardId) {
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch();
+}
+
+flipCard(2);
+flipCard(0);
